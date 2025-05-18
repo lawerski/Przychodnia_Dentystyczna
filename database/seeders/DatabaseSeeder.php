3fb5a98bb54f315f9f68df287ba92efadbc0978d
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-05-18 22:04:17 +0200

    Added database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Coupon;
+use App\Models\Dentist;
+use App\Models\Reservation;
+use App\Models\Review;
+use App\Models\Service;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +18,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = 50;
+        $dentists = 20;
+        $services = 100;
+        $reviews = 200;
+        $coupons = 10;
+        $reservations = 50;
+        if ($users < $dentists) {
+            // There can't be more dentists than users
+            // setting dentists count to users count instead
+            $dentists = $users;
+        }
+        User::factory()->count($users)->create();
+        Dentist::factory()->count($dentists)->withMaxPossibleUserId($users)->create();
+        Service::factory()->count($services)->withMaxPossibleDoctorId($dentists)->create();
+        Review::factory()->count($reviews)->withMaxPossibleUserId($users)->withMaxPossibleDoctorId($dentists)->create();
+        Coupon::factory()->count($coupons)->withMaxPossibleUserId($users)->withMaxPossibleServiceId($services)->create();
+        Reservation::factory()->count($reservations)->withMaxPossibleUserId($users)->withMaxPossibleServiceId($services)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'username' => 'AdminUser',
             'email' => 'test@example.com',
+            'phone' => '1234567890',
+            'password' => bcrypt('1234'), // password
+            'type' => 'admin',
         ]);
     }
 }
