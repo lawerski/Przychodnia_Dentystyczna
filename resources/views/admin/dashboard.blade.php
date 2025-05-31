1ad30321186ffa81597efdf1b6badc620c955634
Michał Kalisiak <kalisiak.m1ch4l@gmail.com>
2025-05-31 18:48:44 +0200

    zadania 15, 16, 17 panele admina, dentysty, pacjetna oraz ich role

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container mt-4">
+    <h2>Panel [rola]</h2>
+    <p>Pusty panel [rola]</p>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,16 @@ Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminPanelController::class, 'index'])->name('dashboard');
+});
+
+Route::middleware(['auth', 'dentist'])->group(function () {
+    Route::get('/dentist', [DentistPanelController::class, 'index'])->name('dashboard');
+});
+
+Route::middleware(['auth', 'patient'])->group(function () {
+    Route::get('/patient', [PatientPanelController::class, 'index'])->name('dashboard');
+});
