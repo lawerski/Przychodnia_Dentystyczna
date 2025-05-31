b3dfa685479d189dad56849ee21b08b75237822f
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-05-31 23:09:38 +0200

    Moved routes into middleware

--- resources/views/shared/dentist/navbar.blade.php
+++ resources/views/shared/dentist/navbar.blade.php
@@ -1,7 +1,6 @@
-{{-- TODO: Add routes --}}
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('dentist.show') }}">Panel dentysty</a>
+        <a class="navbar-brand" href="{{ route('dentist.dashboard') }}">Panel dentysty</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -29,7 +28,7 @@
                     </a>
                     {{-- TODO: Add auth --}}
                     <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item" href="">Panel Dentysty</a></li>
+                        <li><a class="dropdown-item" href="{{ route('dentist.dashboard') }}">Panel Dentysty</a></li>
                         <li><a class="dropdown-item" href="">Wyloguj</a></li>
                     </ul>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,6 @@ Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () 
     Route::resource('users', UserController::class);
 });
 
-Route::controller(DentistPanelController::class)->group(function () {
-    Route::get('/dentist/history', 'history')->name('dentist.history');
-    Route::get('/dentist/upcoming', 'upcoming')->name('dentist.upcoming');
-    Route::get('/dentist', 'show')->name('dentist.show');
-    Route::get('/dentist/reviews', 'reviews')->name('dentist.reviews');
-});
-Route::controller(ReservationController::class)->group(function () {
-    Route::put('/reservation/{reservation}/accept', 'accept')->name('reservation.accept');
-});
-
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
@@ -34,9 +24,17 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin', [AdminPanelController::class, 'index'])->name('admin.dashboard');
 });
 
-// Route::middleware(['auth', 'dentist'])->group(function () {
-//     Route::get('/dentist', [DentistPanelController::class, 'show'])->name('dentist.dashboard');
-// });
+Route::middleware(['auth', 'dentist'])->group(function () {
+    Route::controller(DentistPanelController::class)->group(function () {
+        Route::get('/dentist/history', 'history')->name('dentist.history');
+        Route::get('/dentist/upcoming', 'upcoming')->name('dentist.upcoming');
+        Route::get('/dentist/reviews', 'reviews')->name('dentist.reviews');
+        Route::get('/dentist', 'show')->name('dentist.dashboard');
+    });
+    Route::controller(ReservationController::class)->group(function () {
+        Route::put('/reservation/{reservation}/accept', 'accept')->name('reservation.accept');
+    });
+});
 
 Route::middleware(['auth', 'patient'])->group(function () {
     Route::get('/patient', [PatientPanelController::class, 'index'])->name('patient.dashboard');
